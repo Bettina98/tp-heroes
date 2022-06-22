373df0ea6d34d52179adb2bd4d98094750967e8f
Bettina98 <beona67@gmail.com>
2022-06-22 18:54:54 -0300

    agrega rutas y vistas

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
 
+//console.log(__dirname);
 
-
-
+app.get('/', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'index.html')))
+app.get('/babbage', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'babbage.html')))
+app.get('/berners-lee', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'berners-lee.html')))
+app.get('/clarke', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'clarke.html')))
+app.get('/hamilton', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'hamilton.html')))
+app.get('/hopper', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'hopper.html')))
+app.get('/lovelace', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'lovelace.html')))
+app.get('/turing', (req,rest) => rest.sendFile(path.join(__dirname, 'views', 'turing.html')))
 
 app.listen(port, () => console.log('Server running in http://localhost:' + port));
